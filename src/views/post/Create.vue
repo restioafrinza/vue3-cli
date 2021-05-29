c5d2d1b44bc13c3da4566deb81c094f76e5a1c52
Restio Afrinza <restioa@gmail.com>
2021-05-29 23:44:40 +0700

    view & insert

--- src/views/post/Create.vue
+++ src/views/post/Create.vue
@@ -1,9 +1,98 @@
 <template>
-    <h1>Create</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>TAMBAH POST</h4>
+                        <hr>
+
+                        <form @submit.prevent="store">
+                            <div class="form-group">
+                                <label for="title" class="font-weight-bold">TITLE</label>
+                                <input type="text" class="form-control" v-model="post.title" placeholder="Masukkan Judul Post">
+                                <!-- validation -->
+                                <div v-if="validation.title" class="mt-2 alert alert-danger">
+                                    {{ validation.title[0] }}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="content" class="font-weight-bold">CONTENT</label>
+                                <textarea class="form-control" rows="4" v-model="post.content" placeholder="Masukkan Konten Post"></textarea>
+                                <!-- validation -->
+                                <div v-if="validation.content" class="mt-2 alert alert-danger">
+                                    {{ validation.content[0] }}
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">SIMPAN</button>
+                        </form>                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
 export default {
-    
+
+    setup() {
+
+        //pengenalan form post
+        const post = reactive({
+            title: '',
+            content: ''
+        })
+
+        //pengenalan validasi
+        const validation = ref([])
+
+        //router
+        const router = useRouter()
+
+        //method
+        function store() {
+
+            let title   = post.title
+            let content = post.content
+
+            axios.post('http://localhost:8000/api/post', {
+                title: title,
+                content: content
+            }).then(() => {
+
+                //redirect ke post index
+                router.push({
+                    name: 'post.index'
+                })
+
+            }).catch(error => {
+                //pesan error
+                validation.value = error.response.data
+            })
+
+        }
+
+        //return
+        return {
+            post,
+            validation,
+            router,
+            store
+        }
+
+    }
+
 }
 </script>
+
+<style>
+    body{
+        background: lightgray;
+    }
+</style>
--- src/views/post/Index.vue
+++ src/views/post/Index.vue
@@ -1,9 +1,75 @@
 <template>
-    <h1>Index</h1>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>Data Post</h4>
+                        <hr>
+                        <router-link :to="{name: 'post.create'}" class="btn btn-md btn-success">
+                            TAMBAH POST
+                        </router-link>
+                        <table class="table table-striped table-bordered mt-4">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <td scope="col">Title</td>
+                                    <td scope="col">Content</td>
+                                    <td scope="col">Option</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(post, index) in posts" :key="index">
+                                    <td>{{post.title}}</td>
+                                    <td>{{post.content}}</td>
+                                    <td class="text-center">
+                                        <router-link :to="{name: 'post.edit', params:{id: post.id}}" class="btn btn-sm btn-primary mr-1">
+                                            Edit
+                                        </router-link>
+                                        <button class="btn btn-sm btn-danger ml-1">Delete</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { onMounted, ref} from 'vue'
+
 export default {
-    
+    setup() {
+
+        //inisial
+        let posts = ref([])
+
+        //pemanggilan
+        onMounted(() => {
+
+            // get api dari laravel
+            axios.get('http://127.0.0.1:8000/api/post')
+            .then(response => {
+
+                // penempatan dari post data
+                posts.value = response.data.data
+            }).catch(error => {
+                console.log(error.response.data)
+            })
+        })
+
+        return {
+            posts
+        }
+    }
 }
 </script>
+
+<style>
+    body{
+        background: lightgray;
+    }
+</style>
